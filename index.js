52764d657478244951948baa57e0e23308d16455
Tithi-02 <78081068+Tithi-02@users.noreply.github.com>
2023-04-13 18:28:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello, My name is Tithi')
+  res.send('Hello')
 })
 
 app.listen(4000, ()=>{
